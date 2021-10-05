a598cd1019946daedbb62190febb01820c925bee
Ankit Kumar <ankitkr511@gmail.com>
2021-10-05 17:04:45 +0530

    fix

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   service              = "certbot-lambda"
-  lambda_function_name = var.lambda_name
+  lambda_function_name = join("-", split(".", var.lambda_name))
   log_group_name       = "/aws/lambda/${local.lambda_function_name}"
 }
