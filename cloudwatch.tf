7c896a755ef1f91fb47cad58253fab81e72382d5
Ankit Kumar <ankitkr511@gmail.com>
2021-10-05 16:51:41 +0530

    fix

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -4,7 +4,7 @@ module "cloudwatch_event" {
   rule_name           = "${local.service}-timer-${local.lambda_function_name}"
   rule_description    = "Create a timer that runs every day at 1 AM(UTC)"
   schedule_expression = "cron(0 1 * * ? *)"
-  target_arn          = module.certbot_lambda.function_arn
+  target_arn          = module.certbot_lambda.lambda_function_arn
   rule_tags = {
     "Service" = local.service,
     "Target"  = local.lambda_function_name
@@ -14,7 +14,7 @@ module "cloudwatch_event" {
 module "cloudwatch_lambda_permission" {
   source = "git::git@github.com:vy-labs/vastu-elements.git//aws/lambda/permission?ref=development"
 
-  function_name = module.certbot_lambda.function_name
+  function_name = module.certbot_lambda.lambda_function_name
 
   name       = "${local.service}-${local.lambda_function_name}-cloudwatch-events"
   action     = "lambda:InvokeFunction"
